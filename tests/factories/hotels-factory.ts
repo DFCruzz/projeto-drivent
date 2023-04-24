c9feeff9b04795e94a981b080db9e0d9a192d098
DFCruzz <diegofcruzz@gmail.com>
2023-04-24 15:14:41 -0300

    feat: hotels factory added

---
+++ tests/factories/hotels-factory.ts
@@ -0,0 +1,21 @@
+import faker from '@faker-js/faker';
+import { prisma } from '@/config';
+
+export async function createHotel() {
+  return prisma.hotel.create({
+    data: {
+      name: faker.company.companyName(),
+      image: faker.image.dataUri(),
+    },
+  });
+}
+
+export async function createHotelRoom(hotelId: number) {
+  return prisma.room.create({
+    data: {
+      name: faker.lorem.word(),
+      capacity: faker.datatype.number({ min: 1 }),
+      hotelId: hotelId,
+    },
+  });
+}
